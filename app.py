47c873d1606b80ace46e29375a7ce4a3ab875c55
Enagandula Srinivas <Srinuasv3266@gmail.com>
2026-07-10 07:57:38 -0400

    Added SQL Injection lab

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def home():
 
 @app.route("/login")
 def login():
-    return "Login Page"
+    username = request.args.get("username")
+
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+
+    return query
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
@@ -16,3 +20,5 @@ if __name__ == "__main__":
 import os
 
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+
